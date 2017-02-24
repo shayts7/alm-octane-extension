bd2bbb83c67c203fbf92ca2abb78c8fef6715d88
Uri Kalish <uri.kalish@gmail.com>
2017-02-24 16:08:50 +0200

    prism cont

--- chrome/prism/prism-view.js
+++ chrome/prism/prism-view.js
@@ -9,12 +9,19 @@ angular.module('mainApp').directive('prismView', function () {
 angular.module('mainApp').controller('prismCtrl', function prismCtrl($scope) {
 
 	$scope.model = {
-		title: 'PRISM',
-		showButtonTitle: 'Show',
-		hideButtonTitle: 'Hide',
 		jobs: [],
-		canShow: false,
-		canHide: false
+		addJobName: '',
+		addJobUrl: '',
+		uiStrings: {
+			title: 'PRISM',
+			nameLabel: 'Name:',
+			nameInputHint: 'Enter job nickname/alias...',
+			urlLabel: 'URL:',
+			urlInputHint: 'Enter job console log URL...',
+			addButton: '+',
+			showButton: 'Show',
+			hideButton: 'Hide'
+		}
 	}
 
 	$scope.model.jobs.push({active: true, name: 'Job 1', url: 'http://server:port/build/log'});
@@ -22,4 +29,17 @@ angular.module('mainApp').controller('prismCtrl', function prismCtrl($scope) {
 	$scope.model.jobs.push({active: true, name: 'Job 3', url: 'http://server:port/build/log'});
 	$scope.model.jobs.push({active: true, name: 'Job 4', url: 'http://server:port/build/log'});
 	$scope.model.jobs.push({active: true, name: 'Job 5', url: 'http://server:port/build/log'});
+
+	$scope.canAdd = function canAdd() {
+		return $scope.model.addJobName && $scope.model.addJobUrl;
+	}
+
+	$scope.canShow = function canShow() {
+		return $scope.model.jobs.length > 0;
+	}
+
+	$scope.canHide = function canHide() {
+		return false;
+	}
+
 });
