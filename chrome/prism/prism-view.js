34823ed7a1bcfd08907181b0fb48b135f769293c
Uri Kalish <uri.kalish@gmail.com>
2017-02-24 15:05:59 +0200

    prism view

--- chrome/prism/prism-view.js
+++ chrome/prism/prism-view.js
@@ -9,6 +9,17 @@ angular.module('mainApp').directive('prismView', function () {
 angular.module('mainApp').controller('prismCtrl', function prismCtrl($scope) {
 
 	$scope.model = {
-		title: 'PRISM'
+		title: 'PRISM',
+		showButtonTitle: 'Show',
+		hideButtonTitle: 'Hide',
+		jobs: [],
+		canShow: false,
+		canHide: false
 	}
+
+	$scope.model.jobs.push({active: true, name: 'Job 1', url: 'http://server:port/build/log'});
+	$scope.model.jobs.push({active: true, name: 'Job 2', url: 'http://server:port/build/log'});
+	$scope.model.jobs.push({active: true, name: 'Job 3', url: 'http://server:port/build/log'});
+	$scope.model.jobs.push({active: true, name: 'Job 4', url: 'http://server:port/build/log'});
+	$scope.model.jobs.push({active: true, name: 'Job 5', url: 'http://server:port/build/log'});
 });
