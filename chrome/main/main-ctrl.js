da2c9b99b6f844381d821cca6e2fda03869b62f3
Uri Kalish <uri.kalish@gmail.com>
2017-02-24 02:02:03 +0200

    main popup tabs

--- chrome/main/main-ctrl.js
+++ chrome/main/main-ctrl.js
@@ -1,6 +1,11 @@
 angular.module('mainApp').controller('mainCtrl', function mainCtrl($scope, $document, $timeout, mainServ) {
 
 	$scope.model = {
-		title: 'ALM Octane Extension'
+		title: 'ALM Octane Extension',
+		tabs: [
+			{name: 'General', index: 0, isActive: false},
+			{name: 'Artemis', index: 1, isActive: true},
+			{name: 'Prism', index: 2, isActive: false}
+		]
 	};
 });
