357019839502cc87feb2267e7875a51db924fbeb
Uri Kalish <uri.kalish@hpe.com>
2017-02-26 19:40:34 +0200

    artemis init before every msg

--- chrome/artemis/background.js
+++ chrome/artemis/background.js
@@ -4,16 +4,27 @@
         if (request.type === 'artemis-inject') {
             console.log('artemis-inject');
             let tab = request.msg;
-			tabsToInject.push(tab);
-            //TODO: handle the case when Tab is dead or the user would like to stop injecting
+            let found = false;
+            for (let i = 0; i < tabsToInject.length; i++) {
+				if (tabsToInject[i].id === tab.id) {
+					found = true;
+				}
+			}
+			if (!found) {
+				tabsToInject.push(tab);
+			}
+			//TODO: handle the case when Tab is dead or the user would like to stop injecting
 			console.log('Sending message to tab ' + tab.id + '. type: artemis-msg, msg: init' + request.msg);
 			chrome.tabs.sendMessage(tab.id, {'type': 'artemis-msg', 'msg': 'init'}, function(res){});
         }
         else if (request.type === 'artemis-msg') {
             console.log('artemis-msg');
 			tabsToInject.forEach(function(tab) {
-                console.log('Sending message to tab ' + tab.id + '. type: artemis-msg, msg: ' + request.msg);
-                chrome.tabs.sendMessage(tab.id, {'type': 'artemis-msg', 'msg': request.msg}, function(res){});
+				console.log('Sending message to tab ' + tab.id + '. type: artemis-msg, msg: init' + request.msg);
+				chrome.tabs.sendMessage(tab.id, {'type': 'artemis-msg', 'msg': 'init'}, function(res){
+					console.log('Sending message to tab ' + tab.id + '. type: artemis-msg, msg: ' + request.msg);
+					chrome.tabs.sendMessage(tab.id, {'type': 'artemis-msg', 'msg': request.msg}, function(res){});
+				});
             });
         }
     });
