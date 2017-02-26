810e2a27a6878e7a695e8a91f49331c7fde58b3c
Uri Kalish <uri.kalish@hpe.com>
2017-02-26 18:08:39 +0200

    artemis commands

--- chrome/artemis/background.js
+++ chrome/artemis/background.js
@@ -1,16 +1,18 @@
 (function(){
-    var tabsToInject = [];
+    let tabsToInject = [];
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        //console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension',request.msg);
         if (request.type === 'artemis-inject') {
             console.log('artemis-inject');
-            tabsToInject.push(request.msg);
+            let tab = request.msg;
+			tabsToInject.push(tab);
             //TODO: handle the case when Tab is dead or the user would like to stop injecting
+			console.log('Sending message to tab ' + tab.id + '. type: artemis-msg, msg: init' + request.msg);
+			chrome.tabs.sendMessage(tab.id, {'type': 'artemis-msg', 'msg': 'init'}, function(res){});
         }
         else if (request.type === 'artemis-msg') {
             console.log('artemis-msg');
-            tabsToInject.forEach(function(tab) {
-                console.log('Sending msg ' + request.msg + ' to tab ' + tab.id);
+			tabsToInject.forEach(function(tab) {
+                console.log('Sending message to tab ' + tab.id + '. type: artemis-msg, msg: ' + request.msg);
                 chrome.tabs.sendMessage(tab.id, {'type': 'artemis-msg', 'msg': request.msg}, function(res){});
             });
         }
--- chrome/artemis/content-aut.js
+++ chrome/artemis/content-aut.js
@@ -1,9 +1,12 @@
 (function(){
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
-        alert('request type: ' + request.type + ', request.msg: ' + request.msg);
-        // console.log(sender.tab ?
-        // "from a content script:" + sender.tab.url :
-        //     "from the extension",request);
+        if (request.msg === 'init') {
+			artemisCore.init();
+		} else if (request.msg === 'clean') {
+			artemisCore.clean();
+		} else {
+            let command = JSON.parse(request.msg);
+			artemisCore.command(command);
+        }
     });
 })();
