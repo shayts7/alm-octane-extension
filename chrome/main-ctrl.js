1bfa2c17f4586b356e394e16b11940551b121d77
Hanan Malka <hanan-israel.malka@hpe.com>
2017-02-27 15:18:22 +0200

    Set persistence in popup dialog tabs

--- chrome/main-ctrl.js
+++ chrome/main-ctrl.js
@@ -3,29 +3,37 @@ var mainApp = angular.module('mainApp', [], function() {
 
 angular.module('mainApp').controller('mainCtrl', function mainCtrl($scope) {
 
-	$scope.model = {
-		title: 'ALM Octane Extension',
-		tabs: [
-			{title: 'General', id: 'general'},
-			{title: 'Artemis', id: 'artemis'},
-			{title: 'Prism', id: 'prism'}
-		]
-	};
+  $scope.model = {
+    title: 'ALM Octane Extension',
+    tabs: [
+      {title: 'General', id: 'general'},
+      {title: 'Artemis', id: 'artemis'},
+      {title: 'Prism', id: 'prism'}
+    ]
+  };
 
-	
-	if(localStorage.getItem('activeTab') === ''){
-		$scope.model.activeTabId = $scope.model.tabs[0].id;
-	} else {
-		$scope.model.activeTabId = localStorage.getItem('activeTab');
-	}
+  loadFromLocalStorage();
 
-	$scope.onTabClick = function onTabClick(id) {
-		localStorage.setItem('activeTab', id);
-		$scope.model.activeTabId = id;	
-	}
+  function loadFromLocalStorage() {
+    let str = localStorage.getItem('almOctaneSettings');
+    if (str) {
+      let data = JSON.parse(str);
+      if (data.currentTab) {
+        $scope.model.activeTabId = data.currentTab;
+      }
+    } else {
+      $scope.model.activeTabId = $scope.model.tabs[0].id;
+    }
+  }
 
-	$scope.onCloseClick = function onCloseClick() {
-		window.close();
-	}
+  $scope.onTabClick = function onTabClick(id) {
+    $scope.model.activeTabId = id;
+    let data = {currentTab: $scope.model.activeTabId};
+    localStorage.setItem('almOctaneSettings', JSON.stringify(data));
+  }
+
+  $scope.onCloseClick = function onCloseClick() {
+    window.close();
+  }
 
 });
