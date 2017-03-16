5a19aa14e4bae2b4506c1cdf5d2695cf73c6c248
Hanan Malka <hanan-israel.malka@hpe.com>
2017-03-17 00:44:53 +0200

    Modifications on drop down job list

--- chrome/prism/prism-view.js
+++ chrome/prism/prism-view.js
@@ -6,16 +6,18 @@ angular.module('mainApp').directive('prismView', function() {
   };
 });
 
-angular.module('mainApp').controller('prismCtrl', function prismCtrl($scope, prismManager) {
+angular.module('mainApp').controller('prismCtrl', function prismCtrl($http, $scope, prismManager) {
 
   function loadFromStorage() {
     let data = prismManager.loadFromStorage();
-    $scope.model.jobs = data.jobs || [];
+    $scope.model.uiJobs = data.uiJobs || [];
+    $scope.model.jobList = data.jobs[0].jobList || [];
   }
 
   function saveToStorage() {
     let data = {
-      jobs: $scope.model.jobs
+      jobs: $scope.model.jobs,
+      uiJobs: $scope.model.uiJobs
     };
     prismManager.saveToStorage(data);
   }
@@ -29,60 +31,85 @@ angular.module('mainApp').controller('prismCtrl', function prismCtrl($scope, pri
   $scope.model = {
     jobs: [],
     addJobName: '',
-    addJobUrl: '',
-    addLogUrl: '',
+    addOctaneUrl: '',
+    jobList: '',
     logType: '',
-    addCustomLog: '',
-    addCSSSelectorsLog: '',
     uiStrings: {
       titlePrimary: 'Exploratory Testing',
       titleSecondary: 'UI Automation Coverage',
+      octaneUrlLabel: 'Octane URL:',
+      octaneUrlInputHint: 'Enter Octane URL',
       nameLabel: 'Name:',
       nameInputHint: 'Enter job nickname/alias...',
-      urlLabel: 'URL:',
-      urlInputHint: 'Enter job console log URL...',
-      cssSelectorsInputHint: 'Enter CSS Selectors path...',
-      customParserInputHint: 'Enter Custom JS parser URL...',
       activateCheckboxTooltip: 'show/hide coverage',
       removeButtonTooltip: 'remove',
       showButtonText: 'Show',
       hideButtonText: 'Hide'
     },
+    uiJobs: [],
     parsedCSSRules: '',
     isInProgress: false
   };
 
+  $scope.canRetrieve = function canRetrieve() {
+    return $scope.model.addOctaneUrl;
+
+  }
+
+  $scope.onRetrieveClick = function onRetrieveClick() {
+    let jobsList = [];
+    let req = {
+      method: 'GET',
+      url: 'https://mqast010pngx.saas.hpe.com/api/shared_spaces/1001/workspaces/1002/pipelines?expand=$all{fields=name},author{fields=full_name}&inflate=true&query="(id=51001)"',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'HPECLIENTTYPE': 'HPE_MQM_UI',
+        'HPSSO-HEADER-CSRF': '289bes2bctdbtbeppovmdt37uh'
+      },
+    };
+
+    $http(req).then(function check(response) {
+      let res = response.data;
+      for (let i = 0; i < res.data[0].current_model.structure.phasesInternal.length; i++) {
+        for (let j = 0; j < res.data[0].current_model.structure.phasesInternal[i].jobs.length; j++) {
+          jobsList.push(res.data[0].current_model.structure.phasesInternal[i].jobs[j].name);
+        }
+      }
+      $scope.model.jobs.push({
+        octaneUrl: $scope.model.addOctaneUrl,
+        jobList: jobsList
+      });
+      saveToStorage();
+      $scope.model.addOctaneUrl = '';
+    }
+    );
+  }
+  
+  $scope.canSelectJobs = function canSelectJobs() {
+    return $scope.model.jobs[0].jobList;
+  }
+
   $scope.canAdd = function canAdd() {
-    return $scope.model.addJobName && $scope.model.addJobUrl && $scope.model.logType;
+    return $scope.model.addJobName && $scope.model.logType;
   };
 
   $scope.onAddClick = function onAddClick() {
-    $scope.model.addLogUrl = $scope.model.addCSSSelectorsLog !== '' ? $scope.model.addCSSSelectorsLog : $scope.model.addCustomLog;
-    
-    $scope.model.jobs.push({
+    $scope.model.uiJobs.push({
       active: true,
-      name: $scope.model.addJobName,
-      url: $scope.model.addJobUrl,
-      selectedLogType: $scope.model.logType,
-      logUrl: $scope.model.addLogUrl
+      jobName: $scope.model.jobList,
+      alias: $scope.model.addJobName,
+      selectedLogType: $scope.model.logType
     });
 
     saveToStorage();
     $scope.model.addJobName = '';
-    $scope.model.addJobUrl = '';
-    $scope.model.addCSSSelectorsLog = '';
-    $scope.model.addCustomLog = '';
+    $scope.model.jobList  = '';
   };
-  
-  $scope.onRadioClick = function onRadioClick() {
-    $scope.model.addCSSSelectorsLog = '';
-    $scope.model.addCustomLog = '';
-  }
 
   $scope.onRemoveClick = function onRemoveClick(index) {
-    $scope.model.jobs.splice(index, 1);
+    $scope.model.uiJobs.splice(index, 1);
     saveToStorage();
-    if ($scope.model.jobs.length === 0) {
+    if ($scope.model.uiJobs.length === 0) {
       prismManager.removeColoringFromAUT();
     }
   };
@@ -96,7 +123,6 @@ angular.module('mainApp').controller('prismCtrl', function prismCtrl($scope, pri
     $scope.model.isInProgress = true;
     saveToStorage();
     let activeJobs = getActiveJobs();
-    let logType =
     prismManager.getDataAndColorAUT(activeJobs, getDataAndColorAUTDone);
 
     function getDataAndColorAUTDone() {
